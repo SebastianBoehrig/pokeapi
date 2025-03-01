19c84e08b86836831f908388a643bbfefd600509
SebastianBoehrig <Sebastian.boehrig@gmail.com>
2025-03-01 18:48:31 +0100

    test(backend): tests for get_pokemon_primitive and related
    also found a mistake in the code

--- backend/app/logic.py
+++ backend/app/logic.py
@@ -171,9 +171,9 @@ def _extract_img_from_raw_pokemon_sprites(
         raw_pokemon_sprites,
         {
             'default': Coalesce(
-                'other.official-artwork.front_default', 'sprites.front_default', default=None, skip=None
+                'other.official-artwork.front_default', 'front_default', default=None, skip=None
             ),
-            'shiny': Coalesce('other.official-artwork.front_shiny', 'sprites.front_shiny', default=None, skip=None),
+            'shiny': Coalesce('other.official-artwork.front_shiny', 'front_shiny', default=None, skip=None),
         },
     )
 
--- backend/test/logic_test.py
+++ backend/test/logic_test.py
@@ -1,1 +1,68 @@
-#TODO
+from unittest.mock import patch
+from fastapi import HTTPException
+import pytest
+from app import logic
+
+
+@patch('app.logic.get_pokemon_species')
+@patch('app.logic.get_pokemon')
+def test_get_pokemon_primitive(pokemon_mock, species_mock):
+    re_species = {'varieties': [{'is_default': True, 'pokemon': {'name': 'test_poke_default'}}]}
+    re_poke = {'sprites': {'other': {'official-artwork': {'front_default': 'test_img'}}}}
+    species_mock.return_value = re_species
+    pokemon_mock.return_value = re_poke
+
+    assert logic.get_pokemon_primitive('test_poke') == {'name': 'test_poke', 'img': 'test_img'}
+
+
+@patch('app.logic.get_pokemon_species')
+def test_get_pokemon_primitive_no_varieties(species_mock):
+    species_mock.return_value = {}
+
+    with pytest.raises(HTTPException) as excinfo:
+        logic.get_pokemon_primitive('test_poke')
+    assert str(excinfo.value) == '404: variety of test_poke not found, (get_pokemon_primitive)'
+
+
+@patch('app.logic.get_pokemon_species')
+@patch('app.logic.get_pokemon')
+def test_get_pokemon_primitive_no_sprites(pokemon_mock, species_mock):
+    re_species = {'varieties': [{'is_default': True, 'pokemon': {'name': 'test_poke_default'}}]}
+    species_mock.return_value = re_species
+    pokemon_mock.return_value = {}
+
+    assert logic.get_pokemon_primitive('test_poke') == {'name': 'test_poke', 'img': None}
+
+
+@pytest.mark.parametrize(
+    'input,res',
+    [
+        ([{'is_default': True, 'pokemon': {'name': 'test_poke_default'}}], 'test_poke_default'),
+        ([{'is_default': False, 'pokemon': {'name': 'test_poke_default'}}], None),
+        ([{'is_default': False, 'pokemon': {}}], None),
+        ([{'is_default': True}], None),
+    ],
+)
+def test__extract_default_varietie_name(input, res):
+    assert logic._extract_default_varietie_name(input) == res
+
+
+@pytest.mark.parametrize(
+    'input,res',
+    [
+        (
+            {'other': {'official-artwork': {'front_default': 'test_img', 'front_shiny': 'test_img2'}}},
+            {'default': 'test_img', 'shiny': 'test_img2'},
+        ),
+        (
+            {'front_default': 'test_img', 'front_shiny': 'test_img2'},
+            {'default': 'test_img', 'shiny': 'test_img2'},
+        ),
+        (
+            {'other': {}},
+            {'default': None, 'shiny': None},
+        ),
+    ],
+)
+def test__extract_img_from_raw_pokemon_sprites(input, res):
+    assert logic._extract_img_from_raw_pokemon_sprites(input) == res
